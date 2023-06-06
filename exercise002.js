c608cb5d2fe9095fabb3b19c376e3ea8f36d1da8
Hayley Ashby <hayleyashby7@gmail.com>
2023-06-06 19:36:00 +0100

    Extension with added cats.

--- exercise002.js
+++ exercise002.js
@@ -16,4 +16,11 @@ console.log(await fetchData(jsonTypicode));
 const totallyLegitTechPhrases = 'https://techy-api.vercel.app/api/json';
 
 const response = await fetchData(totallyLegitTechPhrases);
-console.log(`Trust me, I'm an engineer. ${response.message}`);
+console.log(`\nTrust me, I'm an engineer. ${response.message}`);
+
+const catInfoService = 'https://cat-fact.herokuapp.com/facts';
+
+const catResponse = await fetchData(catInfoService);
+const catFact = catResponse[Math.floor(Math.random() * catResponse.length)];
+
+console.log(`\nToday's Cat Fact: ${catFact.text}`);
