73f247da6a0ca9089784766d71ef631792f4b0b9
Hayley Ashby <hayleyashby7@gmail.com>
2023-06-06 19:00:40 +0100

    Converted fetchData into async/await style

--- exercise002.js
+++ exercise002.js
@@ -1,12 +1,14 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 
-const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+const jsonTypicode = 'https://jsonplaceholder.typicode.com/todos/1';
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+const fetchData = async (apiEndPoint) => {
+	try {
+		const response = await fetch(apiEndPoint);
+    console.log(await response.json());    
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 fetchData(jsonTypicode);
